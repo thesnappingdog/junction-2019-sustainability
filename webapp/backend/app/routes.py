a63e597ba478233a47537188c2a82aff480962ae
jsatka <jmksatka@gmail.com>
2019-11-15 21:19:17 +0200

    Add Angular frontend

--- webapp/backend/app/routes.py
+++ webapp/backend/app/routes.py
@@ -1,8 +1,15 @@
 from app import app
-from flask import send_file
+from flask import send_from_directory
 
+FRONTEND_DIST_DIR = '../../frontend/dist'
 
 @app.route('/')
-def index():
-    return send_file('../frontend/dist/index.html', mimetype='text/html')
+def angular_index():
+    return send_from_directory(FRONTEND_DIST_DIR, 'index.html')
 
+# ===== This has to be the VERY LAST route defined in this file! =====
+# If route does not match any route above, then serve frontend file,
+# according to the path
+@app.route("/<path:file_path>", methods=[ "GET" ])
+def send_file(file_path):
+    return send_from_directory(FRONTEND_DIST_DIR, file_path)
