993509f1e256b8d66063a493175224c3df4d5f52
thesnappingdog <richard.a.makara@gmail.com>
2019-11-16 12:42:19 +0200

    DB Migrated, fixed recipe.text column

--- webapp/backend/app/models.py
+++ webapp/backend/app/models.py
@@ -7,7 +7,7 @@ class Recipe(db.Model):
     order_id = db.Column(db.Integer, unique=True)
     name = db.Column(db.String(200))
     image = db.Column(db.String(300))
-    instructions = db.Text()
+    instructions = db.Column(db.Text())
 
     def __repr__(self):
         return f'<Id: {self.recipe_id } Recipe name is {self.name}'
