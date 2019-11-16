3d63ad6f09c236209d769f9922a06f41d232f559
thesnappingdog <richard.a.makara@gmail.com>
2019-11-16 20:41:12 +0200

    routes fix

--- webapp/backend/app/routes.py
+++ webapp/backend/app/routes.py
@@ -15,11 +15,13 @@ def angular_index():
 def rich_recipe_json(recipe_id):
     name, ingredients, instructions, image = get_rich_recipe('00100', int(recipe_id), [])
     return jsonify({
+    return jsonify({'data': {
         'id': recipe_id,
         'name': name,
         'ingredients': ingredients,
         'instructions': instructions,
         'image': image
+    }
     })
 
 # Route for requesting recipes based on items of user
@@ -34,12 +36,14 @@ def infer_recipes_json(items):
 def store_json(zip_code):
     stores = stores_output(zip_code)
     return jsonify(stores)
+    return jsonify({'data': [stores]})
 
 # Route for getting current ingredients of user
 @app.route('/api/possibly_remaining_ingredients/', methods=['GET'])
 def items_json():
     items = default_items()
     return jsonify(items)
+    return jsonify({'data': [items]})
 
 
 # ===== This has to be the VERY LAST route defined in this file! =====
