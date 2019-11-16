7b8fc2f87e4bccbb648166269877728d70305ae7
thesnappingdog <richard.a.makara@gmail.com>
2019-11-16 10:19:29 +0200

    api route expose for rich recipe v1

--- webapp/backend/app/routes.py
+++ webapp/backend/app/routes.py
@@ -1,11 +1,24 @@
 from app import app
-from flask import send_from_directory
+from flask import send_from_directory, jsonify
+from app.api.api import get_rich_recipe
 
 FRONTEND_DIST_DIR = '../../frontend/dist'
+
 
 @app.route('/')
 def angular_index():
     return send_from_directory(FRONTEND_DIST_DIR, 'index.html')
+
+
+@app.route('/api/get_rich_recipe', methods=['GET'])
+def rich_recipe():
+    name, ingredients, instructions, image = get_rich_recipe('00100', 1, [])
+    return jsonify({
+        'name': name,
+        'ingredients': ingredients,
+        'instructions': instructions,
+        'image': image
+    })
 
 # ===== This has to be the VERY LAST route defined in this file! =====
 # If route does not match any route above, then serve frontend file,
